14e1e4c5de040c92c6fcd4b99c8ca3f190314c5b
Michael Drogalis <mjd3089@rit.edu>
2013-05-02 23:56:34 -0400

    Using Pomegranate to resolve dependencies.

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[dire "0.4.2"]]
+  :dependencies [[com.cemerick/pomegranate "0.2.0"]
+                 [dire "0.4.2"]]
   :eval-in-leiningen true)
 
--- src/leiningen/dire.clj
+++ src/leiningen/dire.clj
@@ -1,11 +1,13 @@
 (ns leiningen.dire
-  (:refer-clojure :exclude [read-string])
-  (:require [clojure.edn :refer [read-string]]))
+  (:refer-clojure :exclude [read-string add-classpath])
+  (:require [clojure.edn :refer [read-string]]
+            [cemerick.pomegranate :refer [add-classpath]]))
 
 (defn dire
   "Load the project's Dire load sites into the JVM."
   [project & args]
   (let [deps (clojure.java.io/resource "resources/dire/dependencies.edn")]
+     (apply add-classpath (:source-paths project))
     (doseq [namespace (read-string (slurp deps))]
       (require namespace))))
 
